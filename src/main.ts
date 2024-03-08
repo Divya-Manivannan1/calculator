e307a3d99951aaec3acf75903f24638e023dd54c
Divya-Manivannan1 <reethudya@gmail.com>
2024-03-08 16:03:39 +0000

    added a second display

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,17 @@
 import "./styles/style.scss";
 
-const calculatorOutput = document.querySelector<HTMLTextAreaElement>(
+const calculatorOutput = document.querySelector<HTMLDivElement>(
   ".calculator__output"
 );
+const calculatorInput =
+  document.querySelector<HTMLDivElement>(".calculator__input");
 
-if (!calculatorOutput) {
+if (!calculatorOutput || !calculatorInput) {
   throw new Error("Issue with the selector");
 }
 
 let answer: number = 0;
+let expressionToBeCalculated: string = "";
 
-calculatorOutput.innerText = `${answer}`;
+calculatorOutput.textContent = `${answer}`;
+calculatorInput.textContent = expressionToBeCalculated;
